36905406420d20d9753952f87e93c4161a6c6a11
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-22 16:48:11 +0200

    Create components folder
    Take navigation and header from the  app.js and put them in components. Then use es6 modules and exports the. After that import them into app.js
    The app still work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,56 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
 
 function App() {
   return (
     
     <div>
-      <nav className="side-menu">
-        <ul>
-          <li className="hidden active">
-            <a className="page-scroll" href="#page-top"></a>
-          </li>
-          <li>
-            <a href="#home" className="page-scroll">
-              <span className="menu-title">Home</span>
-              <span className="dot"></span>
-            </a>
-          </li>
-          <li>
-            <a href="#speakers" className="page-scroll">
-              <span className="menu-title">Speakers</span>
-              <span className="dot"></span>
-            </a>
-          </li>
-          <li>
-            <a href="#tickets" className="page-scroll">
-              <span className="menu-title">Buy Tickets</span>
-              <span className="dot"></span>
-            </a>
-          </li>
-          <li>
-            <a href="#schedule" className="page-scroll">
-              <span className="menu-title">Schedule</span>
-              <span className="dot"></span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <Navigation />
       <div className="container-fluid">
         
-        <div className="row hero-header" id="home">
-          <div className="col-md-7">
-            <img src="img/meetup-logo.png" className="logo"/>
-            <h1>Attend the most awaited Conference of 2015</h1>
-            <h3>to start you up with your business!</h3>
-            <h4>20<sup>th</sup> to 22<sup>nd</sup>  October, 2015</h4>
-            <a href="#" className="btn btn-lg btn-red">Buy Tickets Now <span className="ti-arrow-right"></span></a>
-
-          </div>
-          <div className="col-md-5 hidden-xs">
-            <img src="img/rocket.png" className="rocket animated bounce"/>
-          </div>
-        </div>
+        <Header />
              </div>
       <div className="container">
                <div className="row me-row content-ct">
